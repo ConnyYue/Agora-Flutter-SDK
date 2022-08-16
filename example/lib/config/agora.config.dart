f500d5ea3f9e4e1e2da9b42474328cd50cf254b8
yuechen <chen.yue@mena.bee.to>
2022-08-16 21:10:50 +0800

    Run

--- example/lib/config/agora.config.dart
+++ example/lib/config/agora.config.dart
@@ -1,31 +1,42 @@
 /// Get your own App ID at https://dashboard.agora.io/
 String get appId {
   // Allow pass an `appId` as an environment variable with name `TEST_APP_ID` by using --dart-define
-  return const String.fromEnvironment('TEST_APP_ID',
-      defaultValue: '<YOUR_APP_ID>');
+  return const String.fromEnvironment(
+    'ad76720db57e43a99cc063874e258b7b',
+    defaultValue: 'ad76720db57e43a99cc063874e258b7b',
+  );
 }
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
 String get token {
   // Allow pass a `token` as an environment variable with name `TEST_TOKEN` by using --dart-define
-  return const String.fromEnvironment('TEST_TOKEN',
-      defaultValue: '<YOUR_TOKEN>');
+  return const String.fromEnvironment(
+    '006ad76720db57e43a99cc063874e258b7bIACJjtT1pZbBDlwR3DD/bPOk9KxCJZMVzDfR11N6UcqTM6U2fnoAAAAAEAASvOWQt9n8YgEAAQC32fxi',
+    defaultValue:
+        '006ad76720db57e43a99cc063874e258b7bIACJjtT1pZbBDlwR3DD/bPOk9KxCJZMVzDfR11N6UcqTM6U2fnoAAAAAEAASvOWQt9n8YgEAAQC32fxi',
+  );
 }
 
 /// Your channel ID
 String get channelId {
   // Allow pass a `channelId` as an environment variable with name `TEST_CHANNEL_ID` by using --dart-define
   return const String.fromEnvironment(
-    'TEST_CHANNEL_ID',
-    defaultValue: '<YOUR_CHANNEL_ID>',
+    'LiveRoom',
+    defaultValue: 'LiveRoom',
   );
 }
-
-/// Your int user ID
-const int uid = 0;
 
 /// Your user ID for the screen sharing
 const int screenSharingUid = 10;
 
+// /// Your int user ID
+// const int uid = 456789;
+//
+// /// Your string user ID
+// const String stringUid = '456789';
+
+/// Your int user ID
+const int uid = 123456;
+
 /// Your string user ID
-const String stringUid = '0';
+const String stringUid = '123456';
